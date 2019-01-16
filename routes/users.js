00e68093f850f418bc006df575c02ea208a6a49f
“yua <1017123610@qq.com>
2019-01-16 17:47:09 +0800

    new blog

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express')
-const router = express.Router()
-
-router.get('/:name', function (req, res) {
-  res.send('hello, ' + req.params.name)
-})
-
-module.exports = router
